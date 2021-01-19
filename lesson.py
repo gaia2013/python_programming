d2d81af555d0fa6e0742fb066ef07ba308b3a0f3
Hitoshi <hitoshi.gaia.galaxy@gmail.com>
2021-01-19 15:09:15 +0900

    numerical_value

--- lesson.py
+++ lesson.py
@@ -1,14 +1,88 @@
-print('Hi')
-print('Hi', 'Mike', sep=',')
+# print(2+2)
 
-print('Hi', 'Mike', sep=',', end='\n')
-print('Hi', 'Mike', sep=',', end='\n')
+import math
 
-print('Hi', 'Mike', sep=',', end='')
-print('Hi', 'Mike', sep=',', end='')
+result = math.sqrt(25)
+print(result)
 
-print('Hi', 'Mike', sep=',', end='.\n')
-print('Hi', 'Mike', sep=',', end='.\n')
+
+
+###
+
+
+# 15:00:02 hitoshi:~/PycharmProjects/python_programming (section_3_bssics_of_python)$
+#  python
+# Python 3.8.5 (default, Sep  4 2020, 02:22:02)
+# [Clang 10.0.0 ] :: Anaconda, Inc. on darwin
+# Type "help", "copyright", "credits" or "license" for more information.
+# >>> 2+2
+# 4
+# >>> 5 - 2
+# 3
+# >>> 5*6
+# 30
+# >>> 50-5*6
+# 20
+# >>> (50-5)*6
+# 270
+# >>> 8/5
+# 1.6
+# >>> type(1)
+# <class 'int'>
+# >>> type(1.6)
+# <class 'float'>
+# >>> 0.6
+# 0.6
+# >>> .6
+# 0.6
+# >>> 17/3
+# 5.666666666666667
+# >>> 17//3
+# 5
+# >>> 17 % 3
+# 2
+# >>> 5 * 5
+# 25
+# >>> 5**2
+# 25
+# >>> 5*5*5*5*5
+# 3125
+# >>> 5**5
+# 3125
+# >>> x = 5
+# >>> x
+# 5
+# >>> y = 10
+# >>> x * y
+# 50
+# >>> a
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# NameError: name 'a' is not defined
+# >>> pie = 3.1415151515
+# >>> pie
+# 3.1415151515
+# >>> round(pie,2)
+# 3.14
+# >>> exit()
+# (base)
+
+
+
+
+###
+
+# print('Hi')
+# print('Hi', 'Mike', sep=',')
+#
+# print('Hi', 'Mike', sep=',', end='\n')
+# print('Hi', 'Mike', sep=',', end='\n')
+#
+# print('Hi', 'Mike', sep=',', end='')
+# print('Hi', 'Mike', sep=',', end='')
+#
+# print('Hi', 'Mike', sep=',', end='.\n')
+# print('Hi', 'Mike', sep=',', end='.\n')
 
 ###
 
