d7a24a26dcb73b6958caa567a768c9ae776a3ea0
Hitoshi <hitoshi.gaia.galaxy@gmail.com>
2021-01-19 14:54:23 +0900

    variable declaration

--- lesson.py
+++ lesson.py
@@ -1,5 +1,29 @@
-a = 'test'
-b = a
-c = b
+# _num1 = 1
+#
+# # if = 'value'
 
-print(c)
+# num = 1
+# name = '1'
+#
+# new_num = int(name)
+#
+# print(new_num, type(new_num))
+
+
+
+# num = 1
+# name = 'Mike'
+#
+# num =name
+#
+# print(num,type(num))
+
+
+
+# num = 1
+# name = 'Mike'
+# is_ok = True
+#
+# print(num, type(num))
+# print(name, type(name))
+# print(is_ok, type(is_ok))
