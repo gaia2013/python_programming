080ea24e96a3f738ccf627753934dda8cb6a1b60
Hitoshi <hitoshi.gaia.galaxy@gmail.com>
2021-01-20 20:07:36 +0900

    literal index and slice

--- lesson.py
+++ lesson.py
@@ -93,6 +93,40 @@ print(s)
 # aaaaaaaaaaaaaaaaPybbbbbbbbbbbbbbbbbb
 
 
+###
+
+word = 'python'
+print(word[0])
+print(word[1])
+print(word[-1])
+print(word[0:2])
+print(word[2:5])
+print('########')
+print(word[0:2])
+print(word[:2])
+print('########')
+print(word[2:])
+# print(word[100])
+# #     print(word[100])
+# # IndexError: string index out of range
+print('########')
+# word[0] = 'j'
+# #     word[0] = 'j'
+# # TypeError: 'str' object does not support item assignment
+
+word = 'j' + word[1:]
+print(word)
+# jython
+print(word[:])
+# jython
+n = len(word)
+print(n)
+# 6
+
+
+
+
+
 
 ###
 
