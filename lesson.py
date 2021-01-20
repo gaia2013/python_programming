6815f4753a94579ddb5a84b96ab55a928badc258
Hitoshi <hitoshi.gaia.galaxy@gmail.com>
2021-01-20 20:21:41 +0900

    Assigning strings

--- lesson.py
+++ lesson.py
@@ -152,6 +152,45 @@ print(s.lower())
 # my name is mike. hi mike.
 
 print(s.replace('Mike', 'Nancy'))
+###
+
+# $
+#  python
+# Python 3.8.5 (default, Sep  4 2020, 02:22:02)
+# [Clang 10.0.0 ] :: Anaconda, Inc. on darwin
+# Type "help", "copyright", "credits" or "license" for more information.
+# >>> 'a is {}'.format('a')
+# 'a is a'
+# >>> 'a is {}'.format('test')
+# 'a is test'
+# >>> 'a is {} {} {}'.format(1,2,3)
+# 'a is 1 2 3'
+# >>> 'a is {0} {1} {2}'.format(1,2,3)
+# 'a is 1 2 3'
+# >>> 'a is {2} {1} {0}'.format(1,2,3)
+# 'a is 3 2 1'
+# >>> 'My name is {0} {1}'.format('Jun', 'Sakai')
+# 'My name is Jun Sakai'
+# >>> 'My name is {0} {1}. Watashi ha {1} {0}'.format('Jun', 'Sakai')
+# 'My name is Jun Sakai. Watashi ha Sakai Jun'
+# >>> 'My name is {name} {family}. Watashi ha {family} {name}'.format(name='Jun', family='Sakai')
+# 'My name is Jun Sakai. Watashi ha Sakai Jun'
+# >>> 1
+# 1
+# >>> '1'
+# '1'
+# >>> str(1)
+# '1'
+# >>> x = str(1)
+# >>> type(x)
+# <class 'str'>
+# >>> str(3.14)
+# '3.14'
+# >>> str(True)
+# 'True'
+# >>> exit()
+# (base)
+
 
 ###
 
