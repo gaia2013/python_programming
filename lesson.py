74bef3d4aaa69b0d089f094889283e99b2b380ce
Hitoshi <hitoshi.gaia.galaxy@gmail.com>
2021-01-21 12:38:08 +0900

    Tuple Unpacking

--- lesson.py
+++ lesson.py
@@ -727,4 +727,38 @@ print(X)
 # (base)
 
 
+####
+
+
+num_tuple = (10, 20)
+print(num_tuple)
+
+x, y = num_tuple
+print(x, y)
+
+x, y = (10, 20)
+print(x, y)
+
+x, y = 10, 20
+print(x, y)
+
+min, max = 0, 100
+print(min, max)
+
+a, b, c, d, e, f = "Mike", "1", "1", "1", "1", "1"
+a = "Mike"
+b = "1"
+# あまり長めにするのは好まれない
+
+i = 10
+j = 20
+tmp = i
+i = j
+j = tmp
+print(i, j)
+
+a = 100
+b = 200
+a, b = b, a
+print(a, b)
 
