054473b66b8159beb795cea49b78571f12d038a8
Hitoshi <hitoshi.gaia.galaxy@gmail.com>
2021-01-21 21:09:32 +0900

    treat dictionary by for loop

--- lesson.py
+++ lesson.py
@@ -1323,12 +1323,24 @@ print(a is None)
 
 ###
 
-days = ['Mon', 'Tue', 'Wed']
-fruits = ['apple', 'banana', 'orange']
-drinks = ['coffee', 'tea', 'beer']
+# days = ['Mon', 'Tue', 'Wed']
+# fruits = ['apple', 'banana', 'orange']
+# drinks = ['coffee', 'tea', 'beer']
+#
+# # for i in range(len(days)):
+# #     print(days[i], fruits[i], drinks[i])
+#
+# for day, fruit, drink in zip(days, fruits, drinks):
+#     print(day, fruit, drink)
 
-# for i in range(len(days)):
-#     print(days[i], fruits[i], drinks[i])
+###
 
-for day, fruit, drink in zip(days, fruits, drinks):
-    print(day, fruit, drink)
+d = {'x': 100, 'y': 200}
+
+for k, v in d.items():
+    print(k, ':', v)
+# x : 100
+# y : 200
+
+print(d.items())
+# dict_items([('x', 100), ('y', 200)])
