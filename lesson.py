2cc9a376db68a1bf09d9dd5a93fb4e0c23fc68d6
Hitoshi <hitoshi.gaia.galaxy@gmail.com>
2021-01-21 20:25:33 +0900

    for else sentence

--- lesson.py
+++ lesson.py
@@ -1241,30 +1241,48 @@ print(a is None)
 
 
 
-some_list = [1, 2, 3, 4, 5]
+# some_list = [1, 2, 3, 4, 5]
+# #
+# # i = 0
+# # while i < len(some_list):
+# #     print(some_list[i])
+# #     i += 1
 #
-# i = 0
-# while i < len(some_list):
-#     print(some_list[i])
-#     i += 1
+# # iterator 反復処理をするもの
+# for i in some_list:
+#     print(i)
+#
+# for s in 'abcde':
+#     print(s)
+#
+# for word in ['My', 'name', 'is', 'Mike']:
+#     if word == 'name':
+#         continue    # skipする
+#     print(word)
+# # My
+#
+# for word in ['My', 'name', 'is', 'Mike']:
+#     if word == 'name':
+#         continue    # skipする
+#     print(word)
+# # My
+# # is
+# # Mike
 
-# iterator 反復処理をするもの
-for i in some_list:
-    print(i)
 
-for s in 'abcde':
-    print(s)
+###
 
-for word in ['My', 'name', 'is', 'Mike']:
-    if word == 'name':
-        continue    # skipする
-    print(word)
-# My
 
-for word in ['My', 'name', 'is', 'Mike']:
-    if word == 'name':
-        continue    # skipする
-    print(word)
-# My
-# is
-# Mike
+# for fruit in ['apple', 'banana', 'orange']:
+#     print(fruit)
+# else:
+#     print('I ate all')
+
+
+for fruit in ['apple', 'banana', 'orange']:
+    if fruit == 'banana':
+        print("stop eating")
+        break
+    print(fruit)
+else:
+    print('I ate all')
