02c891fcd571f72c6509a1217609f2bebd3abdad
Hitoshi <hitoshi.gaia.galaxy@gmail.com>
2021-01-21 20:30:33 +0900

    range function

--- lesson.py
+++ lesson.py
@@ -1279,10 +1279,35 @@ print(a is None)
 #     print('I ate all')
 
 
-for fruit in ['apple', 'banana', 'orange']:
-    if fruit == 'banana':
-        print("stop eating")
-        break
-    print(fruit)
-else:
-    print('I ate all')
+# for fruit in ['apple', 'banana', 'orange']:
+#     if fruit == 'banana':
+#         print("stop eating")
+#         break
+#     print(fruit)
+# else:
+#     print('I ate all')
+
+
+###
+
+
+
+num_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+for i in num_list:
+    print(i)
+
+for i in range(10):
+    print(i)
+
+for i in range(2, 10):
+    print(i)
+
+for i in range(2, 10, 3):
+    print(i)
+
+for i in range(10):
+    print(i, 'hello')
+
+# _ の場合, index 不使用
+for _ in range(10):
+    print('hello')
