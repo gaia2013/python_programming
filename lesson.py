b57f075193a88ceb67993653069555a2a20d62b9
Hitoshi <hitoshi.gaia.galaxy@gmail.com>
2021-01-21 20:12:16 +0900

    while else sentence

--- lesson.py
+++ lesson.py
@@ -1202,18 +1202,31 @@ print(a is None)
 # while True:
 #     print('XXX')
 
+# count = 0
+# while True:
+#     if count >= 5:
+#         break
+#
+#     if count == 2:
+#         count += 1
+#         continue
+#
+#     print(count)
+#     count += 1
+# # 0
+# # 1
+# # 3
+# # 4
+
+
 count = 0
-while True:
-    if count >= 5:
+
+while count < 5:
+    if count == 1:
         break
-
-    if count == 2:
-        count += 1
-        continue
-
     print(count)
     count += 1
-# 0
-# 1
-# 3
-# 4
+else:
+    print('done')
+
+
