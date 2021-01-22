786f81ade80f18090faaf7f7b64ae6720108eee9
Hitoshi <hitoshi.gaia.galaxy@gmail.com>
2021-01-22 10:32:07 +0900

    Declaring function arguments and return values

--- lesson.py
+++ lesson.py
@@ -1388,4 +1388,17 @@ result = what_is_this('yellow')
 print(result)
 
 
+###
+
+num: int = 10
+
+def add_num(a: int, b: int) -> int:
+    return a + b
+
+r = add_num(10, 20)
+print(r)
+
+r = add_num('a', 'b')
+print(r)
+# error として返してくれず、 ab　を返す
 
