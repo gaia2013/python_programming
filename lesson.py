17f874afd1adcc6fbfe87094df7e95a5187f8b1f
Hitoshi <hitoshi.gaia.galaxy@gmail.com>
2021-01-23 22:01:50 +0900

    set inclusion notation

--- lesson.py
+++ lesson.py
@@ -1780,3 +1780,17 @@ print(d)
 d = {x: y for x, y in zip(w, f)}
 print(d)
 
+
+###
+
+
+s = set()
+
+for i in range(10):
+    if i % 2 == 0:
+        s.add(i)
+
+print(s)
+
+s = {i for i in range(10) if i % 2 == 0}
+print(s)
