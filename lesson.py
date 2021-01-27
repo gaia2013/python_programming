051386607bb5e32c3153a112e16e7f51f89d6524
Hitoshi <hitoshi.gaia.galaxy@gmail.com>
2021-01-27 11:33:22 +0900

    Asterisk import and meaning of __init__.py and __all__.

--- lesson.py
+++ lesson.py
@@ -1,7 +1,12 @@
 # import lesson_package.utils
 # from lesson_package import utils
 # from lesson_package.utils import say_twice
-from lesson_package.talk import human
+# from lesson_package.talk import human
+# from lesson_package.talk import animal
+from lesson_package.talk import *
+
+print(animal.sing())
+print(animal.cry())
 
 # r = lesson_package.utils.say_twice('hello')
 # r = utils.say_twice('hello')
@@ -10,5 +15,5 @@ from lesson_package.talk import human
 
 # print(r)
 
-print(human.sing())
-print(human.cry())
+# print(human.sing())
+# print(human.cry())
