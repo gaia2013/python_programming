7665a60709d2f46a3406af83d997b1fada256663
Hitoshi <hitoshi.gaia.galaxy@gmail.com>
2021-01-27 11:07:26 +0900

    Importing absolute and relative paths

--- lesson.py
+++ lesson.py
@@ -1,11 +1,14 @@
 # import lesson_package.utils
 # from lesson_package import utils
 # from lesson_package.utils import say_twice
-from lesson_package import utils as u
+from lesson_package.talk import human
 
 # r = lesson_package.utils.say_twice('hello')
 # r = utils.say_twice('hello')
 # r = say_twice('hello')
-r = u.say_twice('hello')
+# r = u.say_twice('hello')
 
-print(r)
+# print(r)
+
+print(human.sing())
+print(human.cry())
