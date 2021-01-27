283602b34c921c8b17d04a7b6e62cf56ac21bb85
Hitoshi <hitoshi.gaia.galaxy@gmail.com>
2021-01-27 11:41:43 +0900

    Package and distribute with setup.py

---
+++ dist/python_programming-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='python_programming',
+    version='1.0.0',
+    packages=['lesson_package', 'lesson_package.talk', 'lesson_package.tools'],
+    url='****.com',
+    license='free',
+    author='hitoshi',
+    author_email='python_programming',
+    description=''
+)
