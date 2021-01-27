1d339673c7bbfc2a54e305111093b94bfa77d123
Hitoshi <hitoshi.gaia.galaxy@gmail.com>
2021-01-27 11:36:12 +0900

    How to use import errors

--- lesson.py
+++ lesson.py
@@ -3,10 +3,10 @@
 # from lesson_package.utils import say_twice
 # from lesson_package.talk import human
 # from lesson_package.talk import animal
-from lesson_package.talk import *
+# from lesson_package.talk import *
 
-print(animal.sing())
-print(animal.cry())
+# print(animal.sing())
+# print(animal.cry())
 
 # r = lesson_package.utils.say_twice('hello')
 # r = utils.say_twice('hello')
@@ -17,3 +17,11 @@ print(animal.cry())
 
 # print(human.sing())
 # print(human.cry())
+
+# 新旧パッケージで利用可能にする際に使用
+try:
+    from lesson_package import utils
+except ImportError:
+    from lesson_package.tools import utils
+
+utils.say_twice('word')
