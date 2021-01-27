6ff8da2779469523f6e31a4c8281c1bcf3af751d
Hitoshi <hitoshi.gaia.galaxy@gmail.com>
2021-01-27 11:01:13 +0900

    Import statements and as

--- lesson.py
+++ lesson.py
@@ -1,4 +1,11 @@
-import sys
+# import lesson_package.utils
+# from lesson_package import utils
+# from lesson_package.utils import say_twice
+from lesson_package import utils as u
 
-for i in sys.argv:
-    print(i)
+# r = lesson_package.utils.say_twice('hello')
+# r = utils.say_twice('hello')
+# r = say_twice('hello')
+r = u.say_twice('hello')
+
+print(r)
